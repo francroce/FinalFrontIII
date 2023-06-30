cb383dd21c366a802b5a6861d52a7c8f502ee704
francroce <francocrocee7@gmail.com>
2023-06-30 18:49:02 -0300

    ultima actualizacion

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -4,25 +4,24 @@ import { useParams } from 'react-router-dom'
 const Profile = () => {
 
 const params = useParams()
-const [odontologo,setOdontologo] = useState([])
-async function getOdontologo(){
+const [dentist,setDentist] = useState([])
+async function getDentist(){
     const response= await (await fetch(`https://jsonplaceholder.typicode.com/users/${params.id}`)).json()
-    setOdontologo(response)
+    setDentist(response)
 }
 
 useEffect(() => {
-    getOdontologo()
+  getDentist()
 },[])
-console.log(odontologo)
 
   return (
-    <div className='flex justify-center items-center flex-col w-3/4 mx-auto gap-6 py-10'>
-        <h1 className="font-bold text-4xl text-center">{odontologo.name}</h1>
+    <div className='flex justify-center items-center flex-col w-3/4 mx-auto gap-6 pt-10 pb-20'>
+        <h1 className="font-bold text-4xl text-center">{dentist.name}</h1>
         <div className='w-52 h-52 bg-stone-400'></div>
         <div className='flex flex-col'>
-            <span>{odontologo.email}</span>
-            <span>{odontologo.phone}</span>
-            <span>{odontologo.website}</span>
+            <span>{dentist.email}</span>
+            <span>{dentist.phone}</span>
+            <span>{dentist.website}</span>
         </div>
     </div>
   )
